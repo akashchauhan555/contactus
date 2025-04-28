b482420056dd26860aed796d5d876775f51bc93b
Akash Chauhan <ac1675229@gmail.com>
2025-04-28 13:52:17 +0530

    baseurl add

---
+++ baseUrls.jsx
@@ -0,0 +1,3 @@
+// export const baseURLs = 'https://contactus-6tj7.onrender.com/'
+
+export const baseURLs = 'http://localhost:8000'
--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { toast } from "react-toastify";
+import { baseURLs } from "../baseUrls";
 
 function Contact() {
   const [contact, setContact] = useState({
@@ -15,7 +16,7 @@ function Contact() {
   const submitForm = async (e) => {
     e.preventDefault();
 
-    const response = await fetch("http://localhost:8000/api/auth/contactus", {
+    const response = await fetch(`${baseURLs}/api/auth/contactus`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
